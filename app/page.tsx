208585e5abc5b8d827e9895b1a3046effbd717cb
joy-jeon <ha0913hyun@hanmail.net>
2025-04-17 02:37:18 +0900

    plugin form

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        <input />
         <button className="btn">Submit</button>
       </div>
     </main>
