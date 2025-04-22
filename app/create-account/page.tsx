3face859ae0e6c37a7f8a24738c64939347e62e5
joy-jeon <ha0913hyun@hanmail.net>
2025-04-22 23:08:13 +0900

    import alias 적용(오류해결)

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,7 +1,7 @@
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-import FormInput from "../components/form-input";
-import FormButton from "../components/form-button";
+import FormInput from "@/components/form-input";
+import FormButton from "@/components/form-button";
 
 export default function CreateAccount() {
   return (
