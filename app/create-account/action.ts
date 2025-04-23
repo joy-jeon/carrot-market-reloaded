d261c850cc242b70ff98991cd7313c8bb990443e
joy-jeon <ha0913hyun@hanmail.net>
2025-04-24 00:49:14 +0900

    커스텀 에러문구 , 비밀번호/비밀번호 확인 동시 검증

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,12 +1,33 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(5).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkUsername = (username: string) => !username.includes("potato");
+const checkPassword = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "문자로 입력하세요",
+        required_error:
+          "필수 항목 입니다 ('username'을 찾을 수 없을때 나옴 이름 오타 확인)",
+      })
+      .min(5, "5자 이상 작성")
+      .max(10, "10자 이하로 작성")
+      .refine(checkUsername, "potato를 포함하면 오류"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPassword, {
+    message: "비밀번호가 일치하지 않습니다.",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
